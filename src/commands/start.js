50d22f4813edff59a01d4b047f3fc2eb6a9ecf41
dadsec-dev <geecypher41@gmail.com>
2024-08-21 00:29:26 +0100

    init and architectural setup

---
+++ src/commands/start.js
@@ -0,0 +1,23 @@
+const { tronWeb, encrypt } = require('../utils/tron');
+const { saveUserAddress } = require('../utils/database');
+
+module.exports = async function startCommand(ctx) {
+  try {
+    const account = await tronWeb.createAccount();
+    const encryptedPrivateKey = encrypt(account.privateKey);
+
+    // Save the address and encrypted private key to the database
+    await saveUserAddress(ctx.chat.id, account.address.base58, encryptedPrivateKey);
+
+    const balance = await tronWeb.trx.getBalance(account.address.base58);
+
+    ctx.reply(`
+      Welcome to the TRON Swap Bot!
+      Your new TRON address is: ${account.address.base58}
+      Your TRX balance is: ${tronWeb.fromSun(balance)} TRX
+    `);
+  } catch (error) {
+    console.error("Error creating account or fetching balance:", error);
+    ctx.reply("Sorry, an error occurred while creating your TRON address.");
+  }
+};
